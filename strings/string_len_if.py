eb35f9e3827a34737d6d64f698b087966d0aa097
vitoroso <75452535+vitoleite@users.noreply.github.com>
2020-12-03 19:33:13 -0300

    Add files via upload

---
+++ strings/string_len_if.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+lista1 = ["Maçã","Banana","Laranja"]
+
+qtd = len(lista1)
+print(qtd)
+
+if qtd > 5: 
+	print("qtd é maior que 5")
+elif qtd == 3:
+	print("qtd é exatamente igual a 3")
+elif qtd < 5:
+	print("qtd é menor que 5")
