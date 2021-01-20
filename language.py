4ef687bbd218e247bc5f69b3563c54c84b20c1fe
vitoroso <75452535+vitoleite@users.noreply.github.com>
2021-01-20 10:46:46 -0300

    ok

---
+++ language.py
@@ -0,0 +1,10 @@
+# language list
+language = ['French']
+
+# language tuple
+language_tuple = ('Spanish', 'Portuguese')
+
+# appending language_tuple elements to language
+language.extend(language_tuple)
+
+print('New Language List:', language)
